49d389816dddc4b2e852b42a5723e0d093fa7b9b
Juaria Iqbal <55938363+Juaria@users.noreply.github.com>
2020-03-13 13:11:47 +0600

    Update Random num generation.py

--- Random num generation.py
+++ Random num generation.py
@@ -1,4 +1,4 @@
-// write a funtion that returns a list/array of length n of randomly generated values, from low to high (inclusive)
+# write a funtion that returns a list/array of length n of randomly generated values, from low to high (inclusive)
 
 import random 
 
@@ -14,3 +14,12 @@ num = 5
 start = 0
 end = 20
 print(Rand(start, end, num)) 
+
+
+# Taking input values from the user and displaying it as a string.
+
+values = input("input: ")
+list = values.split(",")
+print('List : ', list)
+
+# for example, input: cat, dog, bird ; output: ['cat' , 'dog', 'bird']
